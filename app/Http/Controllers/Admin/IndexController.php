4b7b351d0059ebbc2d76af1d9d67611bfdc125cb
yangtangxia <1987028242@qq.com>
2019-08-12 10:36:12 +0800

    第三方库

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use QL\QueryList;
 
 // 使用类
 use App\Http\Controllers\Controller;
@@ -12,7 +13,9 @@ class IndexController extends Controller
     //后台首页
     public function  index()
     {
-        return view('admin.index.index');
+        $data = QueryList::get('https://www.58pic.com/piccate/8-0-0.html')->find('img')->attrs('src')->toArray();
+        dump($data);
+//        return view('admin.index.index');
     }
 
 
